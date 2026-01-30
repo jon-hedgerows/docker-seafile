425940d46ea28e846ea7a1f66d8b5d112fca0af4
Jon Davies <jon@hedgerows.org.uk>
2026-01-30 12:24:13 +0000

    better instrumentation

--- predeploy.sh
+++ predeploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo $0 running on $(hostname -f)
+
 # read the docker environment vars
+echo reading environment from .env, $(cat .env | wc -l) lines
 source .env
 
 if test -z "$SEAFILE_VOLUME" ; then
